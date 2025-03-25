f3b8d2cf75101590cb6ad4a646958992a79e5ee9
Lionel Voirol <lionelvoirol@hotmail.com>
2025-03-25 15:30:57 -0500

    added github workflow for pkg check

--- R/test.R
+++ R/test.R
@@ -1,4 +1,9 @@
+#' Print a Test Message
+#'
+#' This function prints a test message to the console.
+#'
+#' @return None.
 #' @export
-my_test_fun = function(){
+my_test_fun <- function() {
   cat("This is a test")
 }
