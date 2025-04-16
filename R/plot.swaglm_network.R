a2a519da049e9d1f71a60d4d5a80b5a39b306b76
yagmuryavuzozdemir <89934860+yagmuryavuzozdemir@users.noreply.github.com>
2025-04-16 12:28:42 -0500

    vertex size scaling is updated

--- R/plot.swaglm_network.R
+++ R/plot.swaglm_network.R
@@ -1,18 +1,20 @@
-
-#' plot.swaglm_network
+#' Plot method for swaglm_network objects
 #'
-#' This function plots a \code{swaglm_network} object.
+#' Visualizes a SWAG network with discretized vertex size, optional edge width scaling,
+#' and edge coloring based on a correlation matrix.
 #'
-#' @param x An object of class \code{swaglm_network}.
-#' @param scale_vertex a \code{double} that specify the size of the nodes of the graph representing variables
-#' @param ... Additional graphical parameters
+#'
+#' @param x An object of class \code{swaglm_network}
+#' @param scale_vertex Optional vertex scaling parameter
+#' @param bins Number of bins for vertex size discretization (default = 5)
+#' @param ... Additional arguments passed to \code{igraph::plot}
 #' @importFrom igraph layout.circle E degree
 #' @importFrom fastglm fastglm
 #' @examples
 #' # Parameters for data generation
 #' set.seed(12345)
 #' n <- 2000
-#' p <- 100
+#' p <- 50
 #' 
 #' # create design matrix and vector of coefficients
 #' Sigma <- diag(rep(1/p, p))
@@ -25,6 +27,8 @@
 #' y <- as.factor(rbinom(n, 1, pr))
 #' y = as.numeric(y)-1
 #' 
+#' cor_matrix = cor(X)
+#' 
 #' # define swag parameters
 #' quantile_alpha = .15
 #' p_max = 20
@@ -32,22 +36,59 @@
 #' alpha = quantile_alpha, verbose = TRUE, seed = 123)
 #' names(swag_obj)
 #' swag_network = compute_network(swag_obj)
-#' plot(swag_network, scale_vertex = .05)
+#' plot(swag_network)
 #' 
 #' @export
-plot.swaglm_network = function(x, scale_vertex = .1, ...){
+#' @export
+#' 
+#
+#' 
+#' 
+library(igraph)
+library(scales)  # for rescale
+library(RColorBrewer)
+library(fields)  # for image.plot
+
+plot.swaglm_network = function(x, bins = 5, scale_edge = NULL, size_range = c(8, 30), ...) {
   
-  if(!inherits(x, "swaglm_network")){
+  if (!inherits(x, "swaglm_network")) {
     stop("Provided object 'x' needs to be of class 'swaglm_network'")
   }
   
+  degs <- igraph::degree(x$g)
   
+  # --- Discretize vertex sizes based on degree
+  degree_bins <- cut(degs,
+                     breaks = quantile(degs, probs = seq(0, 1, length.out = bins + 1), na.rm = TRUE),
+                     include.lowest = TRUE, labels = FALSE)
   
+  size_seq <- seq(size_range[1], size_range[2], length.out = bins)
+  vertex_sizes <- size_seq[degree_bins]
   
-  # x = swag_network
-  plot(x$g_simplified_obs, layout = igraph::layout_in_circle(x$g_simplified_obs) , 
-       vertex.color = "#e3ddff", edge.color = "black",  vertex.size = scale_vertex*igraph::degree(x$g), 
-       edge.width = 0.1*igraph::E(x$g_simplified_obs)$width)
+  # --- Edge width scaling: automatic if not specified
+  edge_weights <- igraph::E(x$g_simplified_obs)$width
+  
+  if (is.null(scale_edge)) {
+    edge_min <- min(edge_weights)
+    edge_max <- max(edge_weights)
+    if (edge_max == edge_min) {
+      edge_scaled <- rep(1, length(edge_weights))  # All equal
+    } else {
+      edge_scaled <- 0.5 + 2.5 * (edge_weights - edge_min) / (edge_max - edge_min)
+    }
+  } else {
+    edge_scaled <- scale_edge * edge_weights
+  }
+  
+  plot(
+    x$g_simplified_obs,
+    layout = igraph::layout_in_circle(x$g_simplified_obs),
+    vertex.color = "#e3ddff",
+    edge.color = "black",
+    vertex.size = vertex_sizes,
+    edge.width = edge_scaled,
+    ...
+  )
 }
 
 
