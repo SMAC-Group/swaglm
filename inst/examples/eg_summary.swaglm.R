61e929a1080f8d22b9bdc6d6c7275782b350018e
Lionel Voirol <lionelvoirol@hotmail.com>
2025-09-10 13:41:13 +0200

    minor

--- inst/examples/eg_summary.swaglm.R
+++ inst/examples/eg_summary.swaglm.R
@@ -23,14 +23,15 @@ swaglm_summ$mat_selected_model
 swaglm_summ$mat_beta_selected_model
 swaglm_summ$mat_p_value_selected_model
 swaglm_summ$vec_aic_selected_model
-
+swaglm_summ$lst_estimated_beta_per_variable
 
 
 # plot distribution of estimated beta with respect to true value
-for(i in seq_along(swaglm_summ$estimated_beta_per_variable)){
+for(i in seq_along(swaglm_summ$lst_estimated_beta_per_variable)){
   # get variable name
-  var_name_i = names(swaglm_summ$estimated_beta_per_variable)[i]
+
+  var_name_i = names(swaglm_summ$lst_estimated_beta_per_variable)[i]
   var_index_i =  as.numeric(substr(var_name_i, 2, nchar(var_name_i)))
-  boxplot(swaglm_summ$estimated_beta_per_variable[[i]], 
+  boxplot(swaglm_summ$lst_estimated_beta_per_variable[[i]], 
           main=paste0(var_name_i, " ", "true value=", beta[i]))
 }
