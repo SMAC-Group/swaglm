6787e72be5c2392127708f0d5f8041512d7cbe23
Lionel Voirol <lionelvoirol@hotmail.com>
2025-09-12 15:44:44 +0200

    update doc

--- R/summary.swaglm.R
+++ R/summary.swaglm.R
@@ -265,30 +265,30 @@ summary.swaglm <- function(object, ...) {
 
 
 
-
-
-
-# # Parameters for data generation
-set.seed(12345)
-n <- 2000
-p <- 100
-# create design matrix and vector of coefficients
-Sigma <- diag(rep(1/p, p))
-X <- MASS::mvrnorm(n = n, mu = rep(0, p), Sigma = Sigma)
-beta = c(-15,-10,5,10,15, rep(0,p-5))
-
-# --------------------- generate from logistic regression with an intercept of one
-z <- 1 + X%*%beta
-pr <- 1/(1 + exp(-z))
-y <- as.factor(rbinom(n, 1, pr))
-y = as.numeric(y)-1
-
-# define swag parameters
-quantile_alpha = .15
-p_max = 20
-x = swaglm::swaglm(X=X, y = y, p_max = p_max, family = stats::binomial(),
-                          alpha = quantile_alpha, verbose = TRUE, seed = 123)
-# test=summary(x)
-# test$mat_selected_model
-# test$lst_estimated_beta_per_variable
-# test$lst_p_value_per_variable
+# 
+# 
+# 
+# # # Parameters for data generation
+# set.seed(12345)
+# n <- 2000
+# p <- 100
+# # create design matrix and vector of coefficients
+# Sigma <- diag(rep(1/p, p))
+# X <- MASS::mvrnorm(n = n, mu = rep(0, p), Sigma = Sigma)
+# beta = c(-15,-10,5,10,15, rep(0,p-5))
+# 
+# # --------------------- generate from logistic regression with an intercept of one
+# z <- 1 + X%*%beta
+# pr <- 1/(1 + exp(-z))
+# y <- as.factor(rbinom(n, 1, pr))
+# y = as.numeric(y)-1
+# 
+# # define swag parameters
+# quantile_alpha = .15
+# p_max = 20
+# x = swaglm::swaglm(X=X, y = y, p_max = p_max, family = stats::binomial(),
+#                           alpha = quantile_alpha, verbose = TRUE, seed = 123)
+# # test=summary(x)
+# # test$mat_selected_model
+# # test$lst_estimated_beta_per_variable
+# # test$lst_p_value_per_variable
